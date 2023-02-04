91b90897f3d6aa619133d4c7dfe374978af29deb
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2023-02-04 15:51:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,25 +15,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 9301845#int(getenv("API_ID"))
+API_HASH = '563e9fd30b529442b705c7230f766b83'#getenv("API_HASH")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "5423234125:AAF3MaDrImAN9TOasM_A6MUNyBP4tNUfR1g"#getenv("BOT_TOKEN")
 
 # SUDO USERS
 SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
+    map(int, getenv("SUDO_USER", "1952992043").split())
 )  # Input type must be interger
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+LOG_GROUP_ID = "-1001785407908"#int(getenv("LOG_GROUP_ID"))
 
 # Message to display when someone starts your bot
-PRIVATE_START_MESSAGE = getenv(
-    "PRIVATE_START_MESSAGE",
-    "Hello! Welcome to my Personal Assistant Bot",
-)
+PRIVATE_START_MESSAGE = "Hello 👋, Thanks for Contacting TMA Support.Please Join our Updates Channel @Tmaadda \n  backup channel @TMABackup. "#getenv(
+  #  "PRIVATE_START_MESSAGE",
+    #"Hello! Welcome to my Personal Assistant Bot",
+#)
 
 # Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = "mongodb+srv://tma:tma@cluster0.3pigxvy.mongodb.net/?retryWrites=true&w=majority"# getenv("MONGO_DB_URI", None)
