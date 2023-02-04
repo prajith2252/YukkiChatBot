0ebe9df0fd7f61b407d6aa5dc899b328366205d4
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2023-02-04 15:56:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ SUDO_USER = list(
 LOG_GROUP_ID = "-1001785407908"#int(getenv("LOG_GROUP_ID"))
 
 # Message to display when someone starts your bot
-PRIVATE_START_MESSAGE = "Hello 👋, Thanks for Contacting TMA Support.Please Join our Updates Channel @Tmaadda \n  backup channel @TMABackup. "#getenv(
+PRIVATE_START_MESSAGE = "Hello 👋, Thanks for Contacting TMA Support.\n\nPlease Join our :- \nUpdates Channel @Tmaadda  \nBackup channel @TMABackup. "#getenv(
   #  "PRIVATE_START_MESSAGE",
     #"Hello! Welcome to my Personal Assistant Bot",
 #)
